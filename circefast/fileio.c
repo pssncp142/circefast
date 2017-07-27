602af4ed2e4b917a5dcf501ab9f892bedc4e739c
pssncp142 <ydallilar@ufl.edu>
2017-07-26 21:54:55 -0400

    cleaner read functions with chatter

--- circefast/fileio.c
+++ circefast/fileio.c
@@ -6,55 +6,85 @@
 
 #include "fileio.h"
 
+#define VERBOSE 1
+
+//print fits object details.
 int fits_info(fitsobj* fitso, char* func){
 
-  #pragma omp critical
-  {
-  int id = omp_get_thread_num();
-  printf("-> Thread %d, I/O func %s\n", id, func);
-  printf("Filename    : %-30s\n", fitso->f_name);
+  char error[30];
   printf("Nramps      : %4d, Ngroups     : %4d\n", 
 	 fitso->nramps, fitso->ngroups);
   printf("Naxis1      : %4d, Naxis2      : %4d\n", 
 	 fitso->naxis1, fitso->naxis2);
-  printf("Status      : %4d\n", fitso->status);
+  fits_get_errstatus(fitso->status, error);
+
+  if (fitso->status > 0){
+    printf("Exiting with status : %4d %s\n", fitso->status, error);
+    exit(0);
   }
 
   return(0);
 }
 
+//initialize fits object structure
+int init_fits_object(fitsfile* fptr, fitsobj* obj, char* f_name, int opt, 
+		     char* func){
+
+  int hdutype;
+  char card[FLEN_CARD];
+  int id = omp_get_thread_num();
+
+  #pragma omp critical
+  {
+  if(VERBOSE) printf("%s[#%d] => Init fits object for : %s\n", 
+		     func, id, f_name);
+
+  strcpy(obj->f_name, f_name);
+  
+  if (opt == 0){
+  //get ngroups and nramps information from PrimaryHDU
+  fits_movabs_hdu(fptr, 1, &hdutype, &obj->status);
+  fits_read_key(fptr, TINT, "NGROUPS", &obj->ngroups, card, &obj->status);
+  fits_read_key(fptr, TINT, "NRAMPS", &obj->nramps, card, &obj->status);
+  } else {
+    obj->ngroups = 2;
+    obj->nramps = 1;
+  }
+
+  if (opt == 0)
+    fits_movabs_hdu(fptr, 2, &hdutype, &obj->status);    
+  else
+    fits_movabs_hdu(fptr, 1, &hdutype, &obj->status);
+  
+  //get the size of the image
+  fits_read_key(fptr, TINT, "NAXIS1", &obj->naxis1, card, &obj->status);
+  fits_read_key(fptr, TINT, "NAXIS2", &obj->naxis2, card, &obj->status);
+
+  //Accordingly set the data size
+  obj->data = malloc(sizeof(float)*obj->naxis1*obj->naxis2*obj->nramps*
+		     (obj->ngroups-1));  
+
+  fits_info(obj, &func[0]);
+
+  }
+  return(1);
+}
 
 //read fits into a fits object
 int read_fits(char *f_name, fitsobj* obj){
 
   char func[100] = "read_fits";
-  int status, hdutype, npixels, anynull;
+  int hdutype, npixels, anynull;
   int nullval = 0;
   fitsfile *fptr;
-  char card[FLEN_CARD];
   int i,j;
   int hdu_num;
 
   obj->status = 0;
+  fits_open_file(&fptr, f_name, READONLY, &obj->status); // get the pointer
+  init_fits_object(fptr, obj, f_name, 0, func);
 
-  fits_open_file(&fptr, f_name, READONLY, &obj->status);
-  
-  fits_movabs_hdu(fptr, 1, &hdutype, &obj->status);
-  fits_read_key(fptr, TINT, "NGROUPS", &obj->ngroups, card, &obj->status);
-  fits_read_key(fptr, TINT, "NRAMPS", &obj->nramps, card, &obj->status);
-
-  fits_movabs_hdu(fptr, 2, &hdutype, &obj->status);
-  fits_read_key(fptr, TINT, "NAXIS1", &obj->naxis1, card, &obj->status);
-  fits_read_key(fptr, TINT, "NAXIS2", &obj->naxis2, card, &obj->status);
-
-  obj->data = malloc(sizeof(float)*obj->naxis1*obj->naxis2*obj->nramps*
-		     (obj->ngroups-1));
   npixels = obj->naxis1*obj->naxis2;
-
-  //obj->status = status;
-  strcpy(obj->f_name, f_name);
-  fits_info(obj, &func[0]);
-  if (obj->status > 0) exit(1);
 
   for (i=0; i<obj->nramps; i++){
     for (j=0; j<obj->ngroups-1; j++){ 
@@ -75,32 +105,20 @@ int read_fits(char *f_name, fitsobj* obj){
 int read_single_fits(char *f_name, fitsobj* obj){
 
   char func[100] = "read_single_fits";
-  int status, hdutype, npixels, anynull;
+  int npixels, anynull;
   int nullval = 0;
   fitsfile *fptr;
-  char card[FLEN_CARD];
 
-  status = 0;
+  obj->status = 0;
+  fits_open_file(&fptr, f_name, READONLY, &obj->status); // get the pointer
+  init_fits_object(fptr, obj, f_name, 1, func);
 
-  fits_open_file(&fptr, f_name, READONLY, &status);
-  
-  fits_movabs_hdu(fptr, 1, &hdutype, &status);
-
-  fits_read_key(fptr, TINT, "NAXIS1", &obj->naxis1, card, &status);
-  fits_read_key(fptr, TINT, "NAXIS2", &obj->naxis2, card, &status);
-
-  obj->data = malloc(sizeof(float)*obj->naxis1*obj->naxis2);
   npixels = obj->naxis1*obj->naxis2;
 
   fits_read_img(fptr, TFLOAT, 1, npixels, &nullval, 
-		&obj->data[0], &anynull, &status);
+		&obj->data[0], &anynull, &obj->status);
 
-  obj->status = status;
-  strcpy(obj->f_name, f_name);
-  fits_info(obj, &func[0]);
-  if (obj->status > 0) exit(1);
-
-  fits_close_file(fptr, &status);
+  fits_close_file(fptr, &obj->status);
   return(0);
 }
 
@@ -108,55 +126,37 @@ int read_single_fits(char *f_name, fitsobj* obj){
 int read_diff_fits(char *f_name, fitsobj* obj){
 
   char func[100] = "read_diff_fits";
-  int status, hdutype, npixels, anynull;
+  int hdutype, npixels, anynull;
   int nullval = 0;
   fitsfile *fptr;
-  char card[FLEN_CARD];
   int i,j;
   int hdu_num;
 
-  status = 0;
+  obj->status = 0;
+  fits_open_file(&fptr, f_name, READONLY, &obj->status); // get the pointer
+  init_fits_object(fptr, obj, f_name, 0, func);
 
-  fits_open_file(&fptr, f_name, READONLY, &status);
-  
-  fits_movabs_hdu(fptr, 1, &hdutype, &status);
-  fits_read_key(fptr, TINT, "NGROUPS", &obj->ngroups, card, &status);
-  fits_read_key(fptr, TINT, "NRAMPS", &obj->nramps, card, &status);
-  
-  fits_movabs_hdu(fptr, 2, &hdutype, &status);
-
-  fits_read_key(fptr, TINT, "NAXIS1", &obj->naxis1, card, &status);
-  fits_read_key(fptr, TINT, "NAXIS2", &obj->naxis2, card, &status);
-
-  obj->data = malloc(sizeof(float)*obj->naxis1*obj->naxis2*obj->nramps*
-		     (obj->ngroups-1));
   obj->data2 = malloc(sizeof(float)*obj->naxis1*obj->naxis2*obj->nramps*
 		     (obj->ngroups-1));
 
   npixels = obj->naxis1*obj->naxis2;
 
-  obj->status = status;
-  strcpy(obj->f_name, f_name);
-  fits_info(obj, &func[0]);
-  if (obj->status > 0) exit(1);
-
   for (i=0; i<obj->nramps; i++){
     for (j=0; j<obj->ngroups-1; j++){
       hdu_num = 2+j+i*(obj->ngroups);
-      fits_movabs_hdu(fptr, hdu_num, &hdutype, &status);
+      fits_movabs_hdu(fptr, hdu_num, &hdutype, &obj->status);
       fits_read_img(fptr, TFLOAT, 1, npixels, &nullval, 
 		    &obj->data[npixels*(i*(obj->ngroups-1)+j)],
-		    &anynull, &status);
-      fits_movabs_hdu(fptr, hdu_num+1, &hdutype, &status);
+		    &anynull, &obj->status);
+      fits_movabs_hdu(fptr, hdu_num+1, &hdutype, &obj->status);
       fits_read_img(fptr, TFLOAT, 1, npixels, &nullval, 
 		    &obj->data2[npixels*(i*(obj->ngroups-1)+j)],
-		    &anynull, &status);
+		    &anynull, &obj->status);
  
     }
   }
 
-  //free(tmp);
-  fits_close_file(fptr, &status);
+  fits_close_file(fptr, &obj->status);
   return(0);
 }
 
